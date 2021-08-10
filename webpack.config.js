b469906e2de4313705478692db802a3af0ecb284
pradeepdsmk <pradeepdsmk@gmail.com>
2021-08-10 22:53:30 +0530

    basic organization chart style tree building by adding nodes

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production', // "production" | "development" | "none"
+    mode: 'development', // "production" | "development" | "none"
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'public'),
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 600,
+        ignored: '**/node_modules'
     }
 };
