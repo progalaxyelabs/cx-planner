93a86e82c8d03eadf7d7d50cfad25388f324f7b6
pradeepdsmk <pradeepdsmk@gmail.com>
2021-08-14 21:29:31 +0530

    rewritten code to use a user-step module to build tree .. for re-usablility. added top-nav with logo and name

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { UserStep } from './user-step'
+
 window.addEventListener('DOMContentLoaded', (e) => {
     App.init()
 })
@@ -7,107 +9,24 @@ var App = (function () {
     let config = null
     let root = null
     let main = null
-    let activeMobileWindow = null;
-    let userStepTemplate = null
 
     let init = function () {
         if (isInitialized) {
             return
         }
 
-        userStepTemplate = document.getElementById('user-step')
-            .content.firstElementChild;
-
         buildTree()
-
-        bindUiActions()
 
         console.log('initialized')
         isInitialized = true
     };
 
-
     function buildTree() {
         main = document.getElementById('main')
-
-        const children = document.createElement('div')
-        children.className = 'children'
-        main.appendChild(children)
-
-        root = createNode()
-
-        children.appendChild(root)
-    }
-
-    function on(name, selector, callback) {
-        main.addEventListener(name, (e) => {
-            const element = e.target
-            if (element.matches(selector)) {
-                return callback(e, element)
-            }
-
-            const ancestorElement = element.closest(selector)
-            if(ancestorElement !== null) {
-                return callback(e, ancestorElement)
-            }
-        })
-    }
-
-    function bindUiActions() {
-        on('click', '.mobile-button.add-action', (event, target) => {            
-            const thisNode = target.closest('.node')
-            createChildNodeTo(thisNode)
-        })     
-        
-        on('click', '.mobile-window', (event, target) => {
-            if (activeMobileWindow) {
-                activeMobileWindow.classList.remove('active')
-            }
-            
-            target.classList.add('active')
-            activeMobileWindow = target
-        })
-    }
-
-    function createNode() {
-        return userStepTemplate.cloneNode(true)
+                
+        root = new UserStep(null)
+        root.createIn('#main')
     }    
-
-    function createChildNodeTo(thisNode) {
-        const childNode = createNode()
-        childNode.querySelector('.vertical-bar.to-parent')
-            .classList.add('active')
-
-        const children = thisNode
-            .querySelector('.children')
-        children.appendChild(childNode)
-
-        thisNode.querySelector('.vertical-bar.to-children')
-            .classList.add('active')
-
-        updateHorizontalBarWidths(thisNode)
-    }
-
-    function updateHorizontalBarWidths(node) {
-        const horizontalBar = node.querySelector('.horizontal-bar')
-        const children = node.querySelector('.children')
-        const numChildren = children.children.length
-        let width = children.offsetWidth
-        const firstChildWidth = children.children[0].offsetWidth
-        const lastChildWidth = children.children[numChildren - 1].offsetWidth
-        const verticalBarWidth = node.querySelector('.vertical-bar').offsetWidth
-        width -= ((firstChildWidth / 2) + (lastChildWidth / 2))
-        width += verticalBarWidth
-
-        horizontalBar.style.width = width + 'px'
-        horizontalBar.style.left = ((firstChildWidth - verticalBarWidth) / 2) + 'px'
-        horizontalBar.classList.add('active')
-
-        const parentNode = node.parentNode.closest('.node')
-        if (parentNode) {
-            updateHorizontalBarWidths(parentNode)
-        }
-    }
 
     return { init }
 })()
---
+++ src/user-step.js
@@ -0,0 +1,445 @@
+
+class UIObject {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        /** @type UserStep */
+        this.parent = _parent
+
+        /** @type {HTMLElement?} */
+        this.element = null
+
+        /** @type {Object?} */
+        this.data = null
+
+        /** @private @type {bool} */
+        this._isHidden = false
+    }
+
+    /** @param {HTMLElement} [element] */
+    setElement(element) {
+        this.element = element
+    }
+
+    hide() {
+        this.element.classList.remove('show')
+        this.element.classList.add('hide')
+        this._isHidden = true
+    }
+
+    show() {
+        this.element.classList.remove('hide')
+        this.element.classList.add('show')
+        this._isHidden = false
+    }
+
+    isHidden() {
+        return this._isHidden
+    }
+
+    on(eventName, callback) {
+        this.element.addEventListener(eventName, callback)
+    }
+
+    activate() {
+        this.element.classList.add('active')
+    }
+
+    deActivate() {
+        this.element.classList.remove('active')
+    }
+
+    focus() {
+        this.element.focus()
+        this.element.scrollIntoView()
+    }
+
+    destroy() {
+        this.parent = null
+        this.element = null
+        this.data = null
+    }
+}
+
+class VerticalBar extends UIObject {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+    }
+
+    getWidth() {
+        return 4
+    }
+}
+
+class VerticalBarToPrevious extends VerticalBar {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.vertical-bar.to-previous')
+    }
+}
+
+class VerticalBarToNext extends VerticalBar {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.vertical-bar.to-next')
+    }
+}
+
+class HorizontalBar extends UIObject {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.horizontal-bar')
+    }
+
+    place(left, width) {
+        this.element.style.left = left + 'px'
+        this.element.style.width = width + 'px'
+    }
+}
+
+class Explanation extends UIObject {
+    static tabIndex = 1
+
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.explanation')
+
+        this.element.tabIndex = Explanation.tabIndex
+        Explanation.tabIndex++        
+    }
+}
+
+class Preview extends UIObject {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.preview')
+    }
+}
+
+class StepToolbarButton extends UIObject {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+    }
+}
+
+class STBAddNextStep extends StepToolbarButton {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.add-next-step')
+
+        this.element.addEventListener('click', (e) => {
+            this.parent.addNextStep()
+        })
+    }
+}
+
+class STBShowNextSteps extends StepToolbarButton {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.show-next-steps')
+
+
+        this.element.addEventListener('click', (e) => {
+            this.parent.showNextSteps()
+        })
+    }
+}
+
+class STBHideNextSteps extends StepToolbarButton {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.hide-next-steps')
+
+        this.element.addEventListener('click', (e) => {
+            this.parent.hideNextSteps()
+        })
+    }
+}
+
+class STBRemoveSelf extends StepToolbarButton {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.remove-self')
+
+        this.element.addEventListener('click', (e) => {
+            const step = this.parent
+            if (!step.parent) {
+                alert('cannot remove this')
+                return
+            }
+
+            if (confirm("Removing this will remove all next steps also. You can not get them back. Are you sure?")) {
+                step.parent.removeNextStep(e)
+            }
+        })
+    }
+}
+
+class StepToolbar extends UIObject {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.step-toolbar')
+
+        this.addNextStep = new STBAddNextStep(_parent)
+        this.showNextSteps = new STBShowNextSteps(_parent)
+        this.hideNextSteps = new STBHideNextSteps(_parent)
+        this.removeSelf = new STBRemoveSelf(_parent)
+    }
+
+    destroy() {
+        this.addNextStep.destroy()
+        this.showNextSteps.destroy()
+        this.hideNextSteps.destroy()
+        this.removeSelf.destroy()
+        super.destroy()
+    }
+}
+class Visual extends UIObject {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.visual')
+
+        this.preview = new Preview(_parent)
+        this.stepToolbar = new StepToolbar(_parent)
+    }
+
+    destroy() {
+        this.preview.destroy()
+        this.stepToolbar.destroy()
+        super.destroy()
+    }
+}
+
+class NextSteps extends UIObject {
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        this.element = _parent.element.
+            querySelector('.next-steps')
+
+        this.userSteps = []
+    }
+
+    getUserStepIndex(element) {
+        let stepElement = element.closest('.step')
+        for (let i = 0; i < this.userSteps.length; i++) {
+            if (this.userSteps[i].element === stepElement) {
+                return i
+            }
+        }
+        return -1
+    }
+
+    getUserStepWidth(index) {
+        return this.userSteps[index].width
+    }
+
+    getFirstStepWidth() {
+        if (this.userSteps.length === 0) {
+            return 0
+        }
+
+        return this.userSteps[0].width
+    }
+
+    getLastStepWidth() {
+        if (this.userSteps.length === 0) {
+            return 0
+        }
+
+        return this.userSteps[this.userSteps.length - 1].width
+    }
+
+    getCount() {
+        return this.userSteps.length
+    }
+
+    getWidth() {
+        let width = 0
+        this.userSteps.forEach((step, index) => {
+            width += step.width
+        })
+
+        return width
+    }
+
+    add(userStep) {
+        this.element.appendChild(userStep.element)
+        this.userSteps.push(userStep)
+    }
+
+    remove(indexToRemove) {
+        this.userSteps[indexToRemove].destroy()
+        this.userSteps.splice(indexToRemove, 1)
+    }
+
+    destroy() {
+        this.userSteps = []
+        super.destroy()
+    }
+}
+
+
+export class UserStep extends UIObject {
+    /** @type {HTMLElement} template */
+    static template = document.getElementById('user-step')
+        .content.firstElementChild
+
+    static paddingX = 4
+    static initialWidth = 208
+
+    /** @param {UserStep} [_parent] */
+    constructor(_parent) {
+        super(_parent)
+
+        const stepElement = UserStep.template.cloneNode(true)
+        this.element = stepElement
+
+        this.verticalBarToParent = new VerticalBarToPrevious(this)
+        this.explanation = new Explanation(this)
+        this.visual = new Visual(this)
+        this.verticalbarToNext = new VerticalBarToNext(this)
+        this.horizontalBar = new HorizontalBar(this)
+        this.nextSteps = new NextSteps(this)
+
+        this.width = UserStep.initialWidth
+    }
+
+    setParent(parent) {
+        throw 'not implemented'
+    }
+
+    createIn(selector) {
+        const el = document.querySelector(selector)
+        if (el) {
+            el.appendChild(this.element)
+        }
+    }
+
+    addNextStep() {
+        const userStep = new UserStep(this)
+        this.nextSteps.add(userStep)
+
+        this.showNextSteps()
+
+        this.verticalbarToNext.show()
+        this.horizontalBar.show()
+        userStep.verticalBarToParent.show()
+
+        // resize at the end
+        this.resize()
+
+        this.explanation.focus()
+    }
+
+    showNextSteps() {
+        this.nextSteps.show()
+        this.verticalbarToNext.show()
+        this.horizontalBar.show()
+        this.resize()
+        this.visual.stepToolbar.showNextSteps.deActivate()
+        this.visual.stepToolbar.hideNextSteps.activate()
+    }
+
+    hideNextSteps() {
+        this.nextSteps.hide()
+        this.verticalbarToNext.hide()
+        this.horizontalBar.hide()
+        this.resize()
+        this.visual.stepToolbar.showNextSteps.activate()
+        this.visual.stepToolbar.hideNextSteps.deActivate()
+    }
+
+    removeNextStep(e) {
+        let element = e.target
+        let indexToRemove = this.nextSteps.getUserStepIndex(element)
+        if (indexToRemove === -1) {
+            return
+        }
+        // make sure you are done with 
+        // getting everything you need from this step
+        // before removing it.
+        // once gone, it's gone forever
+        this.nextSteps.remove(indexToRemove)
+
+        // resize at the end
+        this.resize()
+    }
+
+    resize() {
+
+        const nextStepsWidth = this.nextSteps.getWidth()
+        const nextStepsCount = this.nextSteps.getCount()
+
+        this.width = nextStepsWidth
+
+        if ((nextStepsCount === 0) || (this.nextSteps.isHidden())) {
+            this.width = UserStep.initialWidth
+            this.horizontalBar.place(0, 0)
+            this.verticalbarToNext.hide()
+            this.horizontalBar.hide()
+        } else {
+            const firstStepWidth = this.nextSteps.getFirstStepWidth()
+            const lastStepWidth = this.nextSteps.getLastStepWidth()
+            const vbWidth = this.verticalBarToParent.getWidth()
+            const hbWidth = nextStepsWidth - (firstStepWidth / 2) - (lastStepWidth / 2) + vbWidth
+            const left = (firstStepWidth / 2) - (vbWidth / 2) - UserStep.paddingX
+
+            this.horizontalBar.place(left, hbWidth)
+        }
+
+        if (!this.parent) {
+            return
+        }
+
+        this.parent.resize()
+    }
+
+    destroy() {
+        this.verticalBarToParent.destroy()
+        this.explanation.destroy()
+        this.visual.destroy()
+        this.verticalbarToNext.destroy()
+        this.horizontalBar.destroy()
+        this.nextSteps.destroy()
+
+        this.element.remove()
+
+        super.destroy()
+    }
+}
