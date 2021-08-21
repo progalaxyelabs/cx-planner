54ee3ee3b146e7ace41c862d060ee17a01684abd
pradeepdsmk <pradeepdsmk@gmail.com>
2021-08-21 16:12:49 +0530

    moved style and image files from public to src folder, modified webpack to bundle the css, html and image files separately

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,51 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'production', // "production" | "development" | "none"
-    entry: './src/index.js',
+    entry: {
+        files: [
+            path.resolve(__dirname, './src/index.js'),
+            path.resolve(__dirname, './src/styles.scss')
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                './src/index.html',
+                './src/favicon.ico',
+                './src/site.webmanifest',
+                { from: 'img/**/*', context: path.resolve(__dirname, 'src') }
+            ]
+        })
+    ],
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'public'),
+        filename: 'scripts.js',
+        path: path.resolve(__dirname, './public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+
+                            }
+                        }
+                    }
+                ]
+            },
+        ]
     },
     watch: true,
     watchOptions: {
